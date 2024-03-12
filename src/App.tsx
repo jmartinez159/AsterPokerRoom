10391e99dd0220d8cfb79ccb7618944b43dc2b11
Jorge <jmx012@gmail.com>
2024-03-11 19:11:15 -0700

    Fixed background issue

--- src/App.tsx
+++ src/App.tsx
@@ -4,13 +4,15 @@ import './App.css';
 
 /* Notes:
   - images cant be from local machine(security issue).
+  <img src={'https://img3.goodfon.com/wallpaper/nbig/f/b1/london-england-streets-night-people-urban-scene-cityscape-li.jpg'} alt='Cannot Load' />
 */
 
 function App() {
   return (
     <div className="App">
+      <br></br>
       <h1>Welcome</h1>
-      <img src={'https://img3.goodfon.com/wallpaper/nbig/f/b1/london-england-streets-night-people-urban-scene-cityscape-li.jpg'} alt='Cannot Load' />
+      <p> Poker will be avaliable soon </p>
     </div>
   );
 }
